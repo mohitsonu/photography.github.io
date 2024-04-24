024d1865b724c2a8a83be25bb2c185519b130375
mohitsonu <mohitsonu33@gmail.com>
2024-04-24 14:07:55 +0530

    v1

---
+++ main.js
@@ -0,0 +1,54 @@
+// Preload certain things
+window.onload = () => {
+    document.getElementById('total-bg-images-in-slider').innerText = '03';
+    document.getElementById('bg-image-position-in-slider').innerText = '01';
+}
+
+//-------- Hero Slider Logic -----------
+const hero_bg_images = ['./images/hero1.jpeg', './images/hero2.jpeg', './images/hero3.jpeg'];
+const hero = document.getElementById('hero');
+const heroSliderPreviousBgButton = document.getElementById('get-previous__bg__slider_btn');
+const heroSliderNextBgButton = document.getElementById('get-next__bg__slider_btn');
+const currentBgPositionInHeroSlider = document.getElementById('bg-image-position-in-slider');
+const totalImagesInHeroSlider = document.getElementById('total-bg-images-in-slider');
+
+// Handler to move the slider images backward
+heroSliderPreviousBgButton.onclick = (e) => {
+    let getCurrentBgPosition = (+currentBgPositionInHeroSlider.innerText) - 1; //0-indexed
+    // If we reached to 1st image => then go to last images else decrement the position by 1;
+    if (getCurrentBgPosition === 0){
+        hero.style.background = `url(${hero_bg_images[hero_bg_images.length-1]}) no-repeat center`;
+        currentBgPositionInHeroSlider.innerText = `0${hero_bg_images.length}`;
+    }
+    else {
+        hero.style.background = `url(${hero_bg_images[getCurrentBgPosition-1]}) no-repeat center`;
+        currentBgPositionInHeroSlider.innerText = `0${getCurrentBgPosition}`;
+    }
+    hero.style.backgroundSize = 'cover';
+}
+
+// Handler to move the slider images forward
+heroSliderNextBgButton.onclick = (e) => {
+    let getCurrentBgPosition = (+currentBgPositionInHeroSlider.innerText); //0-indexed
+    // If we reached to last image => then go to 1st images else increment the position by 1;
+    if (getCurrentBgPosition === hero_bg_images.length){
+        hero.style.background = `url(${hero_bg_images[0]}) no-repeat center`;
+        currentBgPositionInHeroSlider.innerText = `01`;
+    }
+    else {
+        hero.style.background = `url(${hero_bg_images[getCurrentBgPosition]}) no-repeat center`;
+        currentBgPositionInHeroSlider.innerText = `0${getCurrentBgPosition+1}`;
+    }
+    hero.style.backgroundSize = 'cover';
+}
+
+// -- Change Top Navigation Background Color on Scroll ----
+window.onscroll = (e) => {
+    let navigationBar = document.getElementById('navigation-bar');
+    console.log(window.scrollY)
+    if (window.scrollY <= 200) {
+        navigationBar.style.backgroundColor = "transparent";
+    } else {
+        navigationBar.style.backgroundColor = "#424242";
+    }
+}
